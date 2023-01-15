729b10c68d2dd22a352ef646fcea3828e795b4df
Shevchenko Andrey <demorest49de@gmail.com>
2023-01-15 22:51:18 +0300

    Default Changelist index.html changed; main.js changed;

--- js/main.js
+++ js/main.js
@@ -2,21 +2,19 @@
 
 
 {
-  const addClass = (element, ...tokens) => {
-    element.classList.add(tokens);
-    const ttt = [1,2,3];
-    console.log(': ',ttt.join('::'), ttt, typeof tokens);
+  const addClass = (element, tokens) => {
+    element.classList.add(...tokens);
   };
 
   const createContainer = () => {
     const container = document.createElement('div');
-    addClass(container, 'container');
+    addClass(container, ['container']);
     return container;
   };
 
   const createSection = () => {
     const section = document.createElement('section');
-    addClass(section, ('list-product'));
+    addClass(section, ['list-product']);
     const container = createContainer();
     section.container = container;
     section.append(container);
@@ -38,7 +36,7 @@
       <span class="card-sum__text">Итоговая стоимость:</span>
     `);
     const cardSumPrice = createElem('span');
-    addClass(cardSumPrice, 'card-sum__price');
+    addClass(cardSumPrice, ['card-sum__price']);
     cardSumPrice.textContent = `$0.00`;
     titleSum.append(cardSumPrice);
     return titleSum;
@@ -46,7 +44,7 @@
 
   const createHeader = (title) => {
     const header = document.createElement('div');
-    addClass(header, 'list-product__header');
+    addClass(header, ['list-product__header']);
     const logo = createLogo(title);
     header.append(logo);
     const titleSum = createTitleSum();
@@ -62,16 +60,16 @@
     const nav = createElem('div');
     addClass(nav, ['list-product__nav', 'nav']);
     const navBlock = createElem('div');
-    addClass(navBlock, 'nav__block');
+    addClass(navBlock, ['nav__block']);
     nav.append(navBlock);
 
     const filterBtn = createElem('button');
-    addClass(filterBtn, 'nav__filter-btn');
+    addClass(filterBtn, ['nav__filter-btn']);
     filterBtn.textContent = 'Фильтр';
     navBlock.append(filterBtn);
 
     const form = createElem('form');
-    addClass(form, 'nav__search');
+    addClass(form, ['nav__search']);
     form.insertAdjacentHTML('beforeend',
       `
         <input class="nav__input" type="search" placeholder="Поиск по наименованию и категории">      
@@ -79,21 +77,21 @@
     navBlock.append(form);
 
     const addItemBtn = createElem('button');
-    addClass(addItemBtn, 'nav__add-btn');
+    addClass(addItemBtn, ['nav__add-btn']);
     addItemBtn.textContent = 'Добавить товар';
     navBlock.append(addItemBtn);
 
-    return nav;
+    return {nav, addItemBtn};
   };
 
   const createWrapper = () => {
     const wrapper = createElem('div');
-    addClass(wrapper, 'list-product__wrapper');
+    addClass(wrapper, ['list-product__wrapper']);
     const table = createElem('table');
-    addClass(table, 'list-product__table');
+    addClass(table, ['list-product__table']);
     wrapper.append(table);
     const thead = createElem('thead');
-    addClass(thead, 'list-product__table-head');
+    addClass(thead, ['list-product__table-head']);
     thead.insertAdjacentHTML('beforeend',
       `
         <tr>
@@ -108,7 +106,7 @@
       </tr>
       `);
     const tbody = createElem('tbody');
-    addClass(tbody, 'list-product__table-body');
+    addClass(tbody, ['list-product__table-body']);
     table.append(thead, tbody);
     table.tbody = tbody;
     return {
@@ -119,7 +117,7 @@
 
   const createFooter = () => {
     const footer = createElem('div');
-    addClass(footer, 'list-product__footer');
+    addClass(footer, ['list-product__footer']);
     footer.insertAdjacentHTML('beforeend',
       `
         <div class="list-product__footer">
@@ -146,20 +144,20 @@
 
   const createMainBlock = () => {
     const mainBlock = createElem('div');
-    addClass(mainBlock, 'list-product__main-block');
-    const nav = createNav();
+    addClass(mainBlock, ['list-product__main-block']);
+    const {nav, addItemBtn} = createNav();
     mainBlock.append(nav);
     const {wrapper, tbody} = createWrapper();
     mainBlock.append(wrapper);
     const footer = createFooter();
     mainBlock.append(footer);
 
-    return {mainBlock, tbody};
+    return {mainBlock, tbody, addItemBtn};
   };
 
   const createRow = ({id, title, price, description, category, discont, count, units, images}) => {
     const tr = createElem('tr');
-    addClass(tr, 'list-product__table-tr');
+    addClass(tr, ['list-product__table-tr']);
 
     tr.innerHTML = `
     <td class="list-product__table-td">${id}</td>
@@ -189,93 +187,112 @@
 
   const createOverlay = () => {
     const overlay = createElem('div');
-    addClass(overlay, ['overlay', 'is-visible']);
-    overlay.insertAdjacentHTML('beforeend', `
-      <div class="add-item__container">
+    // addClass(overlay, ['overlay', 'is-visible']);
+    addClass(overlay, ['overlay']);
+    const addItemBlock = createElem('div');//добавить кодом additem, additemcontainer, <= получить их в переменные после этого добавить adjacent все до формы
+    // а ниже в контейнер доб кнопки и в эддайтем доавбить кнпоки
+    addClass(addItemBlock, ['add-item']);
+    const addItemContainer = createElem('div');
+    addClass(addItemContainer, ['add-item__container']);
+    addItemContainer.insertAdjacentHTML('beforeend', `
         <h1 class="add-item__title">добавить товар</h1>
         <div class="add-item__line"></div>
-        
         <form class="add-item__form" action="https://jsonplaceholder.typicode.com/posts" name="add-item__form"
           id="add-item__form" method="post">
           <fieldset class="add-item__content">
-        <div class="add-item__block add-item__name">
-          <label class="add-item__label" for="add-item__name">
-        наименование
-          </label>
-          <input class="add-item__input" type="text" name="name" id="add-item__name" required>
-        </div>
-        
-        <div class="add-item__block add-item__category">
-          <label class="add-item__label" for="add-item__category">
-          категория
-          </label>
-          <input class="add-item__input" type="text" name="category" id="add-item__category" required>
-        </div>
-        
-        <div class="add-item__block add-item__measure">
-          <label class="add-item__label" for="add-item__measure">
-        еденицы измерения
-          </label>
-          <input class="add-item__input" type="text" name="measure" id="add-item__measure" required>
-        </div>
-        
-        <div class="add-item__discount">
-          <label class="add-item__label" for="add-item__discount">дисконт</label>
-          <div class="add-item__input-set">
-        <input class="add-item__checkbox" type="checkbox">
-        <input class="add-item__input" type="text" name="discount" id="add-item__discount"
-           disabled
-           required>
-          </div>
-        </div>
-        
-        <div class="add-item__block add-item__description">
-          <label class="add-item__label" for="add-item__description">описание</label>
-          <textarea class="add-item__input" rows="5" name="description" 
-        id="add-item__description"></textarea>
-        </div>
-        
-        <div class="add-item__block add-item__quantity"><label class="add-item__label" 
-           for="add-item__quantity">количество</label>
-          <input class="add-item__input" type="number" name="quantity" id="add-item__quantity"
-         required></div>
-        
-        <div class="add-item__block add-item__price"><label class="add-item__label"
-        for="add-item__price">
-          цена
-        </label>
-          <input class="add-item__input" type="number" name="price" id="add-item__price" required>
-        </div>
-        
-        <div class="add-item__add-image-button">
-          <label class="add-item__label-add-image" for="add-item__button-image">добавить
-        изображение</label>
-          <input class="add-item__button add-item__button-image" type="file"
-         id="add-item__button-image"
-         name="image"
-         accept="image/jpeg, image/png">
-        </div>
+            <div class="add-item__block add-item__name">
+              <label class="add-item__label" for="add-item__name">
+                наименование
+              </label>
+              <input class="add-item__input" type="text" name="name" id="add-item__name" required>
+            </div>
+          
+            <div class="add-item__block add-item__category">
+              <label class="add-item__label" for="add-item__category">
+                категория
+              </label>
+              <input class="add-item__input" type="text" name="category" id="add-item__category" required>
+            </div>
+          
+            <div class="add-item__block add-item__measure">
+              <label class="add-item__label" for="add-item__measure">
+                еденицы измерения
+              </label>
+              <input class="add-item__input" type="text" name="measure" id="add-item__measure" required>
+            </div>
+          
+            <div class="add-item__discount">
+              <label class="add-item__label" for="add-item__discount">дисконт</label>
+              <div class="add-item__input-set">
+                <input class="add-item__checkbox" type="checkbox">
+                <input class="add-item__input" type="text" name="discount" id="add-item__discount"
+                  disabled required>
+              </div>
+            </div>  
+          
+            <div class="add-item__block add-item__description">
+              <label class="add-item__label" for="add-item__description">описание</label>
+              <textarea class="add-item__input" rows="5" name="description" 
+              id="add-item__description"></textarea>
+            </div>
+          
+            <div class="add-item__block add-item__quantity"><label class="add-item__label" 
+              for="add-item__quantity">количество</label>
+              <input class="add-item__input" type="number" name="quantity" id="add-item__quantity"
+              required>
+            </div>
+          
+            <div class="add-item__block add-item__price">
+              <label class="add-item__label" for="add-item__price">
+                цена
+              </label>
+              <input class="add-item__input" type="number" name="price" id="add-item__price" required>
+            </div>
+          
+            <div class="add-item__add-image-button">
+              <label class="add-item__label-add-image" for="add-item__button-image">добавить
+                изображение</label>
+              <input class="add-item__button add-item__button-image" type="file"
+                id="add-item__button-image"
+                name="image"
+                accept="image/jpeg, image/png">
+            </div>
           </fieldset>
         </form>
-        
-        <div class="add-item__total-block">
-          <p class="add-item__total">
+    `);
+
+    const addItemTotalBlock = createElem('div');
+    addClass(addItemTotalBlock, ['add-item__total-block']);
+
+    addItemTotalBlock.insertAdjacentHTML('beforeend', `
+      <p class="add-item__total">
         <span class="add-item__total-text">Итоговая стоимость: </span>
         <span class="add-item__total-value">$ 0.00</span>
-          </p>
-          <button form="add-item__form" class="add-item__button add-item__button-item" type="submit">
-        добавить товар
-          </button>
-        </div>
-        
-      </div>
-      <button class="add-item-close-button" type="button">
-        <svg width="20" height="20" viewbox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path d="M2 2L22 22" stroke="#6E6893" stroke-width="3" stroke-linecap="round"/>
-          <path d="M2 22L22 2" stroke="#6E6893" stroke-width="3" stroke-linecap="round"/>
-        </svg>
-      </button>
+      </p>
     `);
+
+    const addItemFormBtn = createElem('button');
+    addClass(addItemFormBtn, ['add-item__button', 'add-item__button-item']);
+    addItemFormBtn.type = 'submit';
+    addItemFormBtn.textContent = 'добавить товар';
+    addItemFormBtn.setAttribute('form', 'add-item__form');
+
+    const addItemCloseBtn = createElem('button');
+    addClass(addItemCloseBtn, ['add-item-close-button']);
+    addItemCloseBtn.type = 'button';
+    addItemCloseBtn.insertAdjacentHTML('beforeend', `
+      <svg width="20" height="20" viewbox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path d="M2 2L22 22" stroke="#6E6893" stroke-width="3" stroke-linecap="round"/>
+        <path d="M2 22L22 2" stroke="#6E6893" stroke-width="3" stroke-linecap="round"/>
+      </svg>
+    `);
+
+    addItemTotalBlock.append(addItemFormBtn);
+    addItemContainer.append(addItemTotalBlock);
+    addItemBlock.append(addItemContainer);
+    addItemBlock.append(addItemCloseBtn);
+    overlay.append(addItemBlock);
+
     return overlay;
   };
 
@@ -284,7 +301,7 @@
     const sectionContainer = section.container;
     const header = createHeader(title);
 
-    const {mainBlock, tbody} = createMainBlock();
+    const {mainBlock, tbody, addItemBtn} = createMainBlock();
     renderItems(tbody);
 
     sectionContainer.append(header);
@@ -293,23 +310,23 @@
     const overlay = createOverlay();
 
     app.append(section, overlay);
+
+    return {addItemBtn, overlay};
   };
 
   const init = (selectorApp, title) => {
     const app = document.querySelector(selectorApp);
-    renderCRM(app, title);
+    const crm = renderCRM(app, title);
 
-    // const {list, logo, btnAdd, overlay, form} = crm;
+    const {addItemBtn, overlay} = crm;
 
     // Функционал
 
-    // const renderItems = (list, goods) => {
-    // };
-    //
-    // const allRow = renderItems(list, window.goods);
+    addItemBtn.addEventListener('click', () => {
+      overlay.classList.remove('is-visible');
+    });
 
-    // const addItem = document.querySelector('.add-item');
-    // const addItemCloseButton = document.querySelector('.add-item-close-button');
+    const addItemCloseButton = document.querySelector('.add-item-close-button');
 
     // btnAdd.addEventListener('click', () => {
     //   overlay.classList.remove('is-visible');
